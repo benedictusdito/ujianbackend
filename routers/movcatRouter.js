e99695e84967fc7abb2dc88870de489f811bfb6f
Benedictusdito <januardidito@gmail.com>
2019-08-06 15:49:49 +0700

    completed

--- routers/movcatRouter.js
+++ routers/movcatRouter.js
@@ -4,30 +4,30 @@ const conn = require("../connection");
 // ADD MOVCAT
 
 router.post("/movcat", (req, res) => {
-  const sql = `INSERT INTO movcat SET ?`;
-  const sql2 = `SELECT id, nama_kategori FROM categories WHERE id= ?`;
-  const data = req.body;
+  const sql = `INSERT INTO movcat(movie_id, category_id) VALUES (${
+    req.body.movie_id
+  }, ${req.body.category_id})`;
+  const sql2 = `SELECT movcat.id, movies.nama_movies, tahun_movies, deskripsi_movies FROM movies 
+  JOIN movcat ON movies.id = movcat.movie_id
+  JOIN categories ON categories.id = movcat.category_id`;
 
   // Insert data
-  conn.query(sql, data, (err, result1) => {
+  conn.query(sql, (err, result) => {
     // Terdapat error ketika insert
-    if (err) {
-      return res.send(err);
-    }
+    if (err) return res.send(err);
 
     //  Read data by user id untuk di kirim sebagai respon
-    conn.query(sql2, result1.insertId, (err, result2) => {
-      if (err) {
-        return res.send(err);
-      }
+    conn.query(sql2, (err, result2) => {
+      if (err) return res.send(err);
+      res.send(result2);
     });
   });
 });
 
 // DELETE MOVCAT
 
-router.delete("/movies/:id", (req, res) => {
-  const sql = `DELETE FROM movies WHERE id = ?`;
+router.delete("/movcat/:id", (req, res) => {
+  const sql = `DELETE FROM movcat WHERE id = ?`;
   const data = req.params.id;
 
   conn.query(sql, data, (err, result) => {
